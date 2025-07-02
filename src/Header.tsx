a6f85f7d1918736761569c1e632ad1f255ba0ff2
tyler the coder <125413796+Teylorrrr@users.noreply.github.com>
2025-07-02 16:18:16 -0300

    Header

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,46 +1,20 @@
 function Header(){
-    return(
-        <header>
-            <nav>
-                <div className="header-links">
-                    <a href="">Central do Vendedor</a>
-                    <a href="">Vender na Shopee</a>
-                    <a href="">Baixe o App</a>
-                    <a href="">Siga-nos no</a>
-                    <a href="">Instagram</a>
-                    <a href="">X</a>
-                    <a href="">TikTok</a>
-                </div>
-                <ul>
-                    <li>
-                        <a href="">Notificações</a>
-                    </li>
-                    <li>
-                        <a href="">Ajuda</a>
-                    </li>
-                    <li>
-                        <a href="">Português (BR)</a>
-                    </li>
-                    <li>
-                        <a href="">Cadastrar</a>
-                    </li>
-                    <li>
-                        <a href="">Entre</a>
-                    </li>
-                </ul>
-            </nav>
-            <div className="header-container">
-                <div>Logo</div>
-                <div>
-                    <input type="text" placeholder="Buscar na Shopee"/>
-                    <button>Pesquisar</button>
-                </div>
-                <div>Carrinho</div>
-            </div>
-        </header>
-    )
-}
-
-
-
+return(
+<header>
+<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0&icon_names=sports_basketball" />
+    <div className="menu-bloco">
+      <a href="#">CAMPEONATOS</a>
+      <a href="#">EQUIPES</a>
+      <a href="#">ATLETAS</a>
+      <a href="#">ESTATÍSTICAS</a>
+    </div>
+     <a><span className="material-symbols-outlined">sports_basketball</span></a>
+    <div className="menu-bloco">
+      <a href="#">OUTROS CAMPEONATOS</a>
+      <a href="#">NOTÍCIAS</a>
+      <a href="#">FOTOS</a>
+      <a href="#">LOJA</a>
+    </div>
+</header>
+)}
 export default Header;
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 // import './index.css'
 import './main.css'
+import './Header.css'
 import Header from './Header.tsx'
 import Container from './Container.tsx'
 
