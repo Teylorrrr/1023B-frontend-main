c744d9135a2d67c3ca40cad8f55ec7d918b6a9b1
tyler the coder <125413796+Teylorrrr@users.noreply.github.com>
2025-07-02 16:42:33 -0300

    footer

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-// import './index.css'
-import './main.css'
+
 import './Header.css'
+import './Container.css'
+import './Footer.css'
 import Header from './Header.tsx'
 import Container from './Container.tsx'
+import Footer from './Footer.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Header/>
     <Container/>
+    <Footer/>
   </StrictMode>,
 )
