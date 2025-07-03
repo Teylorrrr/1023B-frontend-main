7e4ea86c7f9357e634d3e7c5a30ae09448642793
tyler the coder <125413796+Teylorrrr@users.noreply.github.com>
2025-07-03 16:36:29 -0400

    Create Router.ts

---
+++ src/Router.ts
@@ -0,0 +1,7 @@
+
+export const routes = [
+  { path: '/', name: 'Início' },
+  { path: '/componente1', name: 'Componente 1' },
+  { path: '/componente2', name: 'Componente 2' },
+  { path: '/outra', name: 'Outra Página' },
+]
