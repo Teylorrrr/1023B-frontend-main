1564a65f58f14f8185a1fb8fcc89e163fdd97c25
tyler the coder <125413796+Teylorrrr@users.noreply.github.com>
2025-07-07 12:23:46 -0300

    Navegação funiconando, só não tem local pros link irem ainda

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,5 +1,5 @@
 import { Link, useLocation } from 'react-router-dom'
-import { routes } from './Routes'
+import { route, route2} from './Router'
 import './header.css'
 
 function Header() {
@@ -9,35 +9,27 @@ function Header() {
 
   return (
     <header>
-      <link
-        rel="stylesheet"
-        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0&icon_names=sports_basketball"
-      />
-
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0&icon_names=sports_basketball"/>
       <div className="menu-bloco">
-        <a href="#">CAMPEONATOS</a>
-        <a href="#">EQUIPES</a>
-        <a href="#">ATLETAS</a>
-        <a href="#">ESTATÍSTICAS</a>
-      </div>
-
-      <a><span className="material-symbols-outlined">sports_basketball</span></a>
-
-      <div className="menu-bloco">
-        <a href="#">OUTROS CAMPEONATOS</a>
-        <a href="#">NOTÍCIAS</a>
-        <a href="#">FOTOS</a>
-        <a href="#">LOJA</a>
-      </div>
-
-      <div className="menu-bloco">
-        {routes.map(route => (
+        {route.map(route => (
           <Link
             key={route.path}
             to={route.path}
             className={location.pathname === route.path ? 'active' : ''}
           >
             {route.name}
+          </Link>
+        ))}
+      </div>
+      <a><span className="material-symbols-outlined">sports_basketball</span></a>
+         <div className="menu-bloco">
+        {route2.map(route2 => (
+          <Link
+            key={route2.path}
+            to={route2.path}
+            className={location.pathname === route2.path ? 'active' : ''}
+          >
+            {route2.name}
           </Link>
         ))}
       </div>
--- src/Router.ts
+++ src/Router.ts
@@ -1,7 +1,13 @@
 
-export const routes = [
-  { path: '/', name: 'Início' },
-  { path: '/componente1', name: 'Componente 1' },
-  { path: '/componente2', name: 'Componente 2' },
-  { path: '/outra', name: 'Outra Página' },
+export const route = [
+  { path: '/componente1', name: 'CAMPEONATOS' },
+  { path: '/componente2', name: 'EQUIPES' },
+  { path: '/componente2', name: 'ATLETAS' },
+  { path: '/componente2', name: 'ESTATISTICAS' },
+]
+export const route2 = [
+  { path: '/componente1', name: 'OUTROS CAMPEONATOS' },
+  { path: '/componente2', name: 'NOTÍCIAS' },
+  { path: '/componente2', name: 'FOTOS' },
+  { path: '/componente2', name: 'LOJA' },
 ]
