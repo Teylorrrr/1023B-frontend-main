82c7313c159861175923729f0b505846fd3b51c5
tyler the coder <125413796+Teylorrrr@users.noreply.github.com>
2025-07-08 16:17:21 -0300

    salvando aqui porque oque eu vou tentar fazer agora é do balacobaco

--- src/Pages/Atletas.tsx
+++ src/Pages/Atletas.tsx
@@ -1,6 +1,43 @@
+import { useEffect, useState } from 'react'
 import './Atletas.css'
+interface Atleta {
+  id: number
+  nome: string
+  numeroCamisa: number
+  nomeCamisa: string
+  altura: number
+  envergadura: number
+  saltoVertical: number
+  peso: number
+  posicao: string
+  paisOrigem: string
+  estadoOrigem: string
+  cidadeOrigem: string
+  idade: number
+}
 
-function Atletas() {
+function Atletas()  {
+  const [atletas, setAtletas] = useState<Atleta[]>([])
+  const [mensagem, setMensagem] = useState("")
+
+  useEffect(() => {
+    const buscarAtletas = async () => {
+      try {
+        const resposta = await fetch("http://localhost:8000/atletas")
+        if (resposta.status === 200) {
+          const dados = await resposta.json()
+          setAtletas(dados)
+        } else {
+          const erro = await resposta.json()
+          setMensagem(erro.mensagem || "Erro ao carregar atletas")
+        }
+      } catch (erro) {
+        setMensagem("Erro ao conectar com o servidor")
+      }
+    }
+
+    buscarAtletas()
+  }, [])
   return (
     <>
       <h1>Draft 2025</h1>
@@ -12,13 +49,25 @@ function Atletas() {
           <p>O Draft 2025 está programado para o dia 15 de Dezembro de 2025. As equipes estão se preparando para selecionar novos talentos que irão brilhar na liga.</p>
         </div>
  <img src="/Imagens/Draft2025.png" alt="Draft2025" className="Draft2025" />
-        <div className="draft">
-          <h2>Draft 2024</h2>
-          <p>O Draft 2024 ocorreu no dia 10 de janeiro de 2024. Várias promessas foram escolhidas pelas equipes durante o evento de seleção de novos talentos.</p>
-        </div>
       </div>
        <h1>Principais candidatos ao Draft</h1>
-       
+     {mensagem && <p className="mensagem">{mensagem}</p>}
+
+      <div className="container-atletas">
+        {atletas.map(atleta => (
+          <div key={atleta.id} className="card-atleta">
+            <h3>{atleta.nomeCamisa} #{atleta.numeroCamisa}</h3>
+            <p><strong>Nome completo:</strong> {atleta.nome}</p>
+            <p><strong>Idade:</strong> {atleta.idade} anos</p>
+            <p><strong>Altura:</strong> {atleta.altura} m</p>
+            <p><strong>Envergadura:</strong> {atleta.envergadura} m</p>
+            <p><strong>Salto Vertical:</strong> {atleta.saltoVertical} cm</p>
+            <p><strong>Peso:</strong> {atleta.peso} kg</p>
+            <p><strong>Posição:</strong> {atleta.posicao}</p>
+            <p><strong>Origem:</strong> {atleta.cidadeOrigem}, {atleta.estadoOrigem} - {atleta.paisOrigem}</p>
+          </div>
+        ))}
+      </div>
     </>
   )
 }
